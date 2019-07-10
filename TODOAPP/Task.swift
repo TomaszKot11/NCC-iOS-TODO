69c399e4356be47ba9a6d0da3e5b04980acc5a18
Tomasz Kot <kottomasz98@gmail.com>
2019-07-10 10:59:30 +0200

    Added working image view

---
+++ TODOAPP/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  TODOAPP
+//
+//  Created by Tomasz Kot on 10/07/2019.
+//  Copyright © 2019 Tomasz Kot. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    var id: Int
+    var description: String
+    var date: String
+    var imageName: String?
+}
--- TODOAPP/TodosTableViewController.swift
+++ TODOAPP/TodosTableViewController.swift
@@ -9,24 +9,39 @@
 import UIKit
 
 class TodosTableViewController: UITableViewController {
+    
+    var tasks = [
+        Task(id: 0, description: "Wash the dishes", date: "10-07-2019 13:00", imageName: "Apple"),
+        Task(id: 1, description: "Take out the trash", date: "11-07-2019 6:00", imageName: "Apricot"),
+        Task(id: 2, description: "Walk the dog", date: "12-07-2019 11:00", imageName: "Apple"),
+        Task(id: 3, description: "Learn programming language", date: "13-97-2019 12:00")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return tasks.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoListElement", for: indexPath)
-
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-
+        
+        let task = tasks[indexPath.row]
+        
+        cell.textLabel?.text = "Id: \(task.id), description: \(task.description)"
+        cell.detailTextLabel?.text = "when: \(task.date)"
+        
+        //TODO: make images scalled - custom TableViewCell (?)
+        if let imageName = task.imageName {
+            cell.imageView?.image = UIImage(named: imageName)
+        }
+      
         return cell
     }
  
