5c23da9ba69bffced750e45c670ea799912ce9f1
Tomasz Kot <kottomasz98@gmail.com>
2019-07-10 12:37:21 +0200

    Added view and controller for adding tasks

--- TODOAPP/AddTaskViewController.swift
+++ TODOAPP/AddTaskViewController.swift
@@ -1,20 +1,17 @@
 //
-//  ViewController.swift
+//  AddTaskViewController.swift
 //  TODOAPP
 //
 //  Created by Tomasz Kot on 10/07/2019.
 //  Copyright © 2019 Tomasz Kot. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class AddTaskViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
